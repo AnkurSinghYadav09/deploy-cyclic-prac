512fb5d6053f0d364084e9dc55387da7fb4db7be
hilalhussa31 <100183431+AnkurSinghYadav09@users.noreply.github.com>
2024-01-27 21:22:03 +0530

    cyclic

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/welcome", (req, res) => {
+  res.send("welcome to the world of PT-web-16 ");
+});
+
+app.get("/goodbye", (req, res) => {
+  res.send("goodbye this is the last lecture of PT-web-16-backend ");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  try {
+    console.log("listening on port " + PORT);
+  } catch (error) {
+    console.log(error);
+  }
+});
